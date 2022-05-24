859633f2efdd271bdf2b240236b8e0688e3be3fc
David Caldera <davidcaldera13@yahoo.com>
2022-05-24 18:58:46 -0500

    added comment controller functions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,25 @@ class CommentsController < ApplicationController
     render json: comment, status: :created
   end
 
+  def update
+    comment = find_comment
+    comment.update(comment_params)
+    render json: comment
+  end
+
+  def destroy
+    comment = find_comment
+    comment.destroy
+    head :no_content
+  end
+
   private
 
+  def find_comment
+    Comment.find(params[:id])
+  end
+
   def comment_params
-    params.permit(:user_id, :post_id, :content)
+    params.permit(:user_id, :post_id, :content, :id)
   end 
 end
