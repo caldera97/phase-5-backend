5d65e58a2f534a4c44a76c7372cd553b1f12d745
David Caldera <davidcaldera13@yahoo.com>
2022-05-25 12:24:01 -0500

    added user to post serializer attributes

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :img, :tags
+  attributes :id, :content, :user_id, :img, :tags, :user
   belongs_to :user
   has_many :comments
 end
