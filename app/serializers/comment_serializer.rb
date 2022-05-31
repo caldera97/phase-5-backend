43881636e441b5b84e16467bd1d9d0dd66335949
David Caldera <davidcaldera13@yahoo.com>
2022-05-31 11:49:29 -0500

    specified user serializer in comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :user_id, :post_id, :user
-  has_one :user
+  has_one :user, Serializer: UserSerializer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_184422) do
+ActiveRecord::Schema.define(version: 2022_05_31_160846) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2022_05_18_184422) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
